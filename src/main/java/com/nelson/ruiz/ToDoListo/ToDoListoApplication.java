fe9955284969a8eeafc59941388f34224860db08
NelssRUix <nelrum2003uc@gmail.com>
2024-09-18 17:16:05 -0500

    first commit

---
+++ src/main/java/com/nelson/ruiz/ToDoListo/ToDoListoApplication.java
@@ -0,0 +1,13 @@
+package com.nelson.ruiz.ToDoListo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoListoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nelson/ruiz/ToDoListo/ToDoListoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nelson.ruiz.ToDoListo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
