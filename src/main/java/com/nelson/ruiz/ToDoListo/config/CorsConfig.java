a9bb5514bdbcae98a3741f1dd1f730ed79d8cfb1
NelssRUix <nelrum2003uc@gmail.com>
2024-10-02 17:33:36 -0500

    add cors

---
+++ src/main/java/com/nelson/ruiz/ToDoListo/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.nelson.ruiz.ToDoListo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig  implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // Orígenes permitidos
+                .allowedMethods("GET", "POST", "PUT","DELETE") // Métodos específicos
+                .allowedHeaders("Content-Type") // Encabezados específicos
+                .exposedHeaders("Content-Disposition") // Exponer encabezados
+                .allowCredentials(true)
+                .maxAge(3600); // Cache de CORS por 1 hora
+    }
+
+
+}
